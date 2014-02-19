e0919166fe06680cd595e2c5c3b313740fe4234a
Ryan Clark <ryan.clark.j@gmail.com>
2014-02-19 15:04:34 -0700

    support for gpg or openssl encryption

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ var inquirer = require('inquirer');
 var fs = require('fs');
 var path = require('path');
 var AWS = require('aws-sdk');
-var ursa = require('ursa');
-var gpg = require('gpg');
-var env = {};
+var async = require('async');
+var env = {}, crypto;
 
 var config = module.exports;
 
 // Allow override of the default superenv credentials
-config.setCredentials = function (accessKeyId, secretAccessKey, secureKey) {
+config.setCredentials = function(accessKeyId, secretAccessKey, secureKey) {
     env.accessKeyId = accessKeyId;
     env.secretAccessKey = secretAccessKey;
     env.secureKey = secureKey;
+    crypto = /.gpg$/.exec(secureKey || '') ? require('./lib/gpg-crypto') : require('./lib/rsa-crypto');
 };
 
 // Run configuration wizard on a CFN template.
 config.configure = function(template, stackname, region, overrides, callback) {
     var params = _(template.Parameters).map(_(config.question).partial(overrides));
-    if (env.secureKey) console.log('Encrypting NoEcho params using ' + env.secureKey);
+
     inquirer.prompt(params, function(answers) {
         callback(null, {
             StackName: stackname,
@@ -32,11 +32,23 @@ config.configure = function(template, stackname, region, overrides, callback) {
 // Return a inquirer-compatible question object for a given CFN template
 // parameter.
 config.question = function(overrides, parameter, key) {
+    var filter = !parameter.NoEcho || !crypto.encrypt ?
+        function(value) { return value.toString(); } :
+        function(value) {
+            var done = this.async();
+            crypto.encrypt(value, function(err, result) {
+                if (err) throw err; // no access to a callback that might handle this
+                done(result);
+            });
+        };
+
     var question = {
         name: key,
         message: key + '. ' + parameter.Description || key,
-        filter: parameter.NoEcho ? encryptedInput : function(value) { return value.toString() }
+        filter: filter,
+        choices: parameter.AllowedValues
     };
+
     if ('Default' in parameter) question.default = parameter.Default;
     if (key in overrides.defaults) question.default = overrides.defaults[key];
     if (key in overrides.choices) question.choices = overrides.choices[key];
@@ -45,11 +57,9 @@ config.question = function(overrides, parameter, key) {
 
     question.type = (function() {
         if (parameter.NoEcho === 'true') return 'password';
-        if (parameter.AllowedValues) return 'list';
+        if (question.choices) return 'list';
         return 'input';
     })();
-
-    question.choices = parameter.AllowedValues;
 
     return question;
 };
@@ -58,19 +68,24 @@ config.readTemplate = function(filepath, callback) {
     readJsonFile('template', filepath, callback);
 }
 
-config.readConfiguration = function (filepath, callback) {
-    readJsonFile('configuration', filepath, function (err, configuration) {
+config.readConfiguration = function(filepath, callback) {
+    readJsonFile('configuration', filepath, function(err, configuration) {
         if (err) return callback(err);
 
-        if (env.secureKey) {
-            var secure = ursa.createPrivateKey(fs.readFileSync(env.secureKey));
-            configuration.Parameters = _(configuration.Parameters).reduce(function (memo, value, key) {
-                memo[key] = value.indexOf('secure::') === 0 ? decryptedInput(value) : value;
-                return memo;
-            }, {});
-        }
+        var pairs = _(configuration.Parameters).pairs();
+        async.reduce(pairs, configuration.Parameters, function(memo, pair, callback) {
+            var key = pair[0], value = pair[1];
 
-        callback(null, configuration);
+            if (crypto.prefix && value.indexOf(crypto.prefix) === 0) {
+                crypto.decrypt(value, function(err, result) {
+                    memo[key] = result;
+                    callback(err, memo);
+                });
+            } else { callback(null, memo); }
+        }, function(err, result) {
+            configuration.Parameters = result;
+            callback(err, configuration);
+        });
     });
 }
 
@@ -79,11 +94,11 @@ config.readStackParameters = function(stackname, region, callback) {
         region: region
     }));
 
-    cfn.describeStacks({StackName: stackname}, function (err, data) {
+    cfn.describeStacks({StackName: stackname}, function(err, data) {
         if (err) return callback(err);
         if (data.Stacks.length < 1) return callback(new Error('Stack ' + stackname + ' not found'));
 
-        var params = data.Stacks[0].Parameters.reduce(function (memo, param) {
+        var params = data.Stacks[0].Parameters.reduce(function(memo, param) {
             memo[param.ParameterKey] = param.ParameterValue;
             return memo;
         }, {});
@@ -107,30 +122,44 @@ config.writeConfiguration = function(filepath, config, callback) {
 // Reusable function for determining configuration
 //
 // `options` object should include:
-// - template: Required. Path to the Cloudformation template
+// - template: Required. Path to the CloudFormation template
 // - region: The AWS region to deploy into
-// - name: Required. Name of the Cloudformation stack
+// - name: Required. Name of the CloudFormation stack
 // - config: Optional. Path to a configuration file to use
 // - update: Defaults to false. Reads existing stack parameters.
 // - defaults, choices, messages, filters: Optional. Any of these properties can be
-//   set to an object where the keys are Cloudformation parameter names, and the
+//   set to an object where the keys are CloudFormation parameter names, and the
 //   values are as described by https://github.com/SBoudrias/Inquirer.js#question
 //
 //   Prioritization of defaults written by multiple processes follows:
-//   1. Values set by parameters in an existing Cloudformation stack
+//   1. Values set by parameters in an existing CloudFormation stack
 //   2. Values set by higher-level libs (i.e. passed into this function as options.defaults)
 //   3. Values set by a configuration file
-//   4. Values set by the Cloudformation template
+//   4. Values set by the CloudFormation template
 config.configStack = function(options, callback) {
     options.defaults = options.defaults || {};
     config.readTemplate(options.template, function(err, template) {
         if (err) return callback(err);
 
-        if (!options.config) return afterFileLoad({});
-        config.readConfiguration(options.config, function(err, configuration) {
-            if (err) return callback(err);
-            afterFileLoad(configuration.Parameters);
-        });
+        if (!env.secureKey) return afterPassphrasePrompt({});
+
+        inquirer.prompt([{
+            type: 'password',
+            name: 'passphrase',
+            message: 'Your key\'s passphrase',
+            default: null
+        }], afterPassphrasePrompt);
+
+        function afterPassphrasePrompt(answer) {
+            if (_(answer).has('passphrase')) crypto = crypto(env.secureKey, answer.passphrase);
+
+            if (!options.config) return afterFileLoad({});
+
+            config.readConfiguration(options.config, function(err, configuration) {
+                if (err) return callback(err);
+                afterFileLoad(configuration.Parameters);
+            });
+        }
 
         function afterFileLoad(fileParameters) {
             if (!options.update) return afterStackLoad(fileParameters, {});
@@ -175,73 +204,38 @@ config.configStack = function(options, callback) {
 
 config.createStack = function(options, callback) {
     // `options` object should include
-    // - template: Required. Path to the Cloudformation template
+    // - template: Required. Path to the CloudFormation template
     // - region: The AWS region to deploy into
-    // - name: Required. Name of the Cloudformation stack
+    // - name: Required. Name of the CloudFormation stack
     // - config: Optional. Path to a configuration file to use
     // - iam: Defaults to false. Allows stack to create IAM resources
 
-    var cfn = new AWS.CloudFormation(_(env).extend({
-        region: options.region
-    }));
-
-    config.configStack(options, function (err, configDetails) {
+    deployPrep(options, function(err, cfn, configuration) {
+        if (err && err == 'Canceled') return callback();
         if (err) return callback(err);
-
-        confirmAction('Ready to create this stack?', function (confirm) {
-            if (!confirm) return callback();
-            cfn.createStack({
-                StackName: options.name,
-                TemplateBody: JSON.stringify(configDetails.template, null, 4),
-                Parameters: _(configDetails.configuration.Parameters).map(function(value, key) {
-                    return {
-                        ParameterKey: key,
-                        ParameterValue: value.indexOf('secure::') === 0 ? decryptedInput(value) : value
-                    };
-                }),
-                Capabilities: options.iam ? [ 'CAPABILITY_IAM' ] : []
-            }, callback);
-        });
+        cfn.createStack(configuration, callback);
     });
 };
 
 config.updateStack = function(options, callback) {
     // Same options as createStack above.
 
-    var cfn = new AWS.CloudFormation(_(env).extend({
-        region: options.region
-    }));
-
-    options.update = true;
-    config.configStack(options, function(err, configDetails) {
+    deployPrep(options, function(err, cfn, configuration) {
+        if (err && err == 'Canceled') return callback();
         if (err) return callback(err);
-
-        confirmAction('Ready to update the stack?', function (confirm) {
-            if (!confirm) return callback();
-            cfn.updateStack({
-                StackName: options.name,
-                TemplateBody: JSON.stringify(configDetails.template, null, 4),
-                Parameters: _(configDetails.configuration.Parameters).map(function(value, key) {
-                    return {
-                        ParameterKey: key,
-                        ParameterValue: value.indexOf('secure::') === 0 ? decryptedInput(value) : value
-                    };
-                }),
-                Capabilities: options.iam ? [ 'CAPABILITY_IAM' ] : []
-            }, callback);
-        });
+        cfn.updateStack(configuration, callback);
     });
 }
 
 config.deleteStack = function(options, callback) {
     // `options` object should include
-    // - name: Required. Name of the Cloudformation stack
+    // - name: Required. Name of the CloudFormation stack
     // - region: The AWS region to deploy into
     var cfn = new AWS.CloudFormation(_(env).extend({
         region: options.region
     }));
 
-    confirmAction('Ready to delete the stack ' + options.name + '?', function (confirm) {
+    confirmAction('Ready to delete the stack ' + options.name + '?', function(confirm) {
         if (!confirm) return callback();
         cfn.deleteStack({
             StackName: options.name
@@ -251,7 +245,7 @@ config.deleteStack = function(options, callback) {
 
 config.stackInfo = function(options, callback) {
     // `options` object should include
-    // - name: Required. Name of the Cloudformation stack
+    // - name: Required. Name of the CloudFormation stack
     // - region: The AWS region to deploy into
     // - resources: Defaults to false. Gets information about resources in the stack
     var cfn = new AWS.CloudFormation(_(env).extend({
@@ -300,6 +294,49 @@ function readJsonFile(filelabel, filepath, callback) {
     });
 }
 
+function deployPrep(options, callback) {
+    var cfn = new AWS.CloudFormation(_(env).extend({
+        region: options.region
+    }));
+
+    config.configStack(options, stackConfigured);
+
+    function stackConfigured(err, configDetails) {
+        if (err) return callback(err);
+
+        confirmAction('Ready to push configuration to CloudFormation?', function(confirm) {
+            if (!confirm) return callback();
+
+            var originalParams = configDetails.configuration.Parameters;
+            var pairs = _(originalParams).pairs();
+            async.reduce(pairs, originalParams, processParameter, afterProcessed);
+        });
+
+        function processParameter(memo, pair, callback) {
+            var key = pair[0], value = pair[1];
+            if (!crypto.prefix || value.indexOf(crypto.prefix) !== 0) return callback(null, memo);
+            
+            crypto.decrypt(value, function(err, result) {
+                memo[key] = result;
+                callback(err, memo);
+            });
+        }
+
+        function afterProcessed(err, result) {
+            if (err) return callback(err);
+
+            callback(null, cfn, {
+                StackName: options.name,
+                TemplateBody: JSON.stringify(configDetails.template, null, 4),
+                Parameters: _(result).map(function(value, key) {
+                    return { ParameterKey: key, ParameterValue: value };
+                }),
+                Capabilities: options.iam ? [ 'CAPABILITY_IAM' ] : []
+            });
+        }
+    }
+}
+
 function confirmAction(message, callback) {
     inquirer.prompt([{
         type: 'confirm',
@@ -309,16 +346,4 @@ function confirmAction(message, callback) {
     }], function(answers) {
         callback(answers.confirm);
     });
-}
-
-function encryptedInput(value) {
-    if (!env.secureKey) return value.toString();
-    var secure = ursa.createPrivateKey(fs.readFileSync(env.secureKey));
-    return ['secure', secure.encrypt(value, 'utf8', 'base64')].join('::');
-}
-
-function decryptedInput(value) {
-    if (!env.secureKey) return value.toString();
-    var secure = ursa.createPrivateKey(fs.readFileSync(env.secureKey));
-    return secure.decrypt(value.split('::')[1], 'base64', 'utf8'); 
 }
