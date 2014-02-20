eb14043a46e64a21f40e2d111f90526372a6dcd2
Ryan Clark <ryan.clark.j@gmail.com>
2014-02-20 10:57:50 -0700

    make sure you can delete a DELETE_FAILED stack

--- index.js
+++ index.js
@@ -230,8 +230,9 @@ config.deleteStack = function(options, callback) {
 
     cfn.describeStacks({ StackName: options.name }, function (err, data) {
         if (err) return callback(err);
-        if (data.Stacks[0].StackStatus.indexOf('DELETE_') === 0) {
-            return callback(new Error(options.name + ' is being deleted already'));
+        var status = data.Stacks[0].StackStatus;
+        if (status === 'DELETE_COMPLETE' || status === 'DELETE_IN_PROGRESS') {
+            return callback(new Error([options.name, status].join(' ')));
         }
         
         confirmAction('Ready to delete the stack ' + options.name + '?', function (confirm) {
