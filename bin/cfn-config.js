4c9f3970153709ea7c300b138d08305ce1bf9ef2
Ryan Clark <ryan.clark.j@gmail.com>
2016-08-11 11:40:55 -0700

    cli tests, cli default value for --template-bucket

--- bin/cfn-config.js
+++ bin/cfn-config.js
@@ -1,54 +1,15 @@
 #!/usr/bin/env node
 
-var path = require('path');
-var meow = require('meow');
-var commands = require('../lib/commands');
+var cli = require('../lib/cli');
 
-var cli = meow(`
-  USAGE: cfn-prompt <command> <environment> [templatePath] [options]
+var parsed;
+try { parsed = cli.parse(process.argv.slice(2), process.env); }
+catch (err) { return finished(err); }
 
-  command: one of the following
-    - create                create a new stack
-    - update                update an existing stack
-    - delete                delete an existing stack
-    - info                  fetch information about an existing stack
-    - save                  save an existing stack's configuration
+cli.main(parsed, finished);
 
-  environment:
-    The stack's name is constructed as name-environment
-
-  templatePath:
-    Relative path to the CloudFormation template in JSON format, required for create and update
-
-  options:
-    -n, --name              the stack's base name (default: current directory name)
-    -r, --region            the stack's region (default: us-east-1)
-    -c, --config-bucket     an S3 bucket for storing stack configurations
-    -t, --template-bucket   an S3 bucket in the same region as the stack for storing templates
-    -k, --kms               a KMS key ARN for configuration encryption at rest
-`, {
-  alias: { c: 'config-bucket', t: 'template-bucket', n: 'name', r: 'region' },
-  string: ['config-bucket', 'template-bucket', 'name', 'region'],
-  default: {
-    name: path.basename(process.cwd()),
-    region: 'us-east-1'
-  }
-});
-
-var command = cli.input[0];
-var environment = cli.input[1];
-var templatePath = path.resolve(cli.input[2]);
-function callback(err, data) {
-  if (err) return process.stderr.write(err.message + '\n');
+function finished(err, data) {
+  if (err) process.stderr.write(err.message + '\n');
   if (data) process.stdout.write(JSON.stringify(data, null, 2) + '\n');
+  process.exit(err ? 1 : 0);
 }
-
-var available = commands(cli.flags);
-if (!available[command]) return cli.showHelp();
-
-var args = [environment];
-if (command === 'create' || command === 'update') args.push(templatePath);
-if (command === 'save') args.push(cli.flags.kms);
-args.push(callback);
-
-available[command].apply(null, args);
---
+++ test/cli.test.js
@@ -0,0 +1,299 @@
+var path = require('path');
+var test = require('tape');
+var sinon = require('sinon');
+var cli = require('../lib/cli');
+var cfnConfig = require('..');
+
+test('[cli.parse] aliases and defaults', function(assert) {
+  var args = ['create', 'testing', 'relative/path', '-c', 'config', '-t', 'template'];
+  var parsed = cli.parse(args, {});
+
+  assert.equal(parsed.command, 'create', 'contains command');
+  assert.equal(parsed.environment, 'testing', 'contains environment');
+  assert.equal(parsed.templatePath, path.resolve('relative/path'), 'contains absolute template path');
+  assert.deepEqual(parsed.options, {
+    e: false,
+    extended: false,
+    f: false,
+    force: false,
+    k: false,
+    kms: false,
+    n: path.basename(process.cwd()),
+    name: path.basename(process.cwd()),
+    r: 'us-east-1',
+    region: 'us-east-1',
+    t: 'template',
+    templateBucket: 'template',
+    c: 'config',
+    configBucket: 'config'
+  }, 'provided expected options');
+  assert.deepEqual(parsed.overrides, { force: false }, 'provided expected overrides');
+  assert.ok(parsed.help, 'provides help text');
+  assert.end();
+});
+
+test('[cli.parse] sets options', function(assert) {
+  var args = [
+    'create', 'testing', 'relative/path',
+    '-c', 'config',
+    '-t', 'template',
+    '-e', '-f',
+    '-k', 'kms-id',
+    '-n', 'my-stack',
+    '-r', 'eu-west-1'
+  ];
+
+  var parsed = cli.parse(args, {});
+
+  assert.deepEqual(parsed.options, {
+    e: true,
+    extended: true,
+    f: true,
+    force: true,
+    k: 'kms-id',
+    kms: 'kms-id',
+    n: 'my-stack',
+    name: 'my-stack',
+    r: 'eu-west-1',
+    region: 'eu-west-1',
+    t: 'template',
+    templateBucket: 'template',
+    c: 'config',
+    configBucket: 'config'
+  }, 'provided expected options');
+  assert.deepEqual(parsed.overrides, { force: true }, 'provided expected overrides');
+
+  assert.end();
+});
+
+test('[cli.parse] kms options', function(assert) {
+  var parsed = cli.parse(['info', 'testing', '-k', true], {});
+  assert.equal(parsed.options.kms, 'alias/cloudformation', '--kms as a true flag --> default key id');
+
+  parsed = cli.parse(['info', 'testing', '-k', 'my-key'], {});
+  assert.equal(parsed.options.kms, 'my-key', '--kms as a flag with a value --> provided key id');
+
+  parsed = cli.parse(['info', 'testing'], {});
+  assert.equal(parsed.options.kms, false, '--kms defaults to false');
+
+  assert.end();
+});
+
+test('[cli.parse] handles default template bucket on create & update', function(assert) {
+  var parsed = cli.parse(['info', 'testing'], {});
+  assert.notOk(parsed.options.templateBucket, 'not set when not needed');
+
+  parsed = cli.parse(['create', 'testing'], { AWS_ACCOUNT_ID: '123456789012' });
+  assert.equal(parsed.options.templateBucket, 'cfn-config-templates-123456789012-us-east-1', 'uses default for create');
+
+  parsed = cli.parse(['update', 'testing'], { AWS_ACCOUNT_ID: '123456789012' });
+  assert.equal(parsed.options.templateBucket, 'cfn-config-templates-123456789012-us-east-1', 'uses default for create');
+
+  assert.throws(
+    function() { cli.parse(['create', 'testing'], {}); },
+    /Provide \$AWS_ACCOUNT_ID as an environment variable to use the default template bucket, or set --template-bucket/,
+    'throws error on create without $AWS_ACCOUNT_ID'
+  );
+
+  assert.throws(
+    function() { cli.parse(['update', 'testing'], {}); },
+    /Provide \$AWS_ACCOUNT_ID as an environment variable to use the default template bucket, or set --template-bucket/,
+    'throws error on update without $AWS_ACCOUNT_ID'
+  );
+
+  assert.end();
+});
+
+var base = {
+  command: 'create',
+  environment: 'testing',
+  templatePath: '/my/template',
+  help: 'help text',
+  overrides: { force: false },
+  options: {
+    extended: false,
+    force: false,
+    kms: false,
+    name: 'my-stack',
+    region: 'us-east-1',
+    templateBucket: 'template',
+    configBucket: 'config'
+  }
+};
+
+test('[cli.main] no command', function(assert) {
+  var parsed = Object.assign({}, base, { command: undefined });
+
+  cli.main(parsed, function(err) {
+    assert.equal(err.message, 'Error: invalid command\n\n' + parsed.help, 'expected error message');
+    assert.end();
+  });
+});
+
+test('[cli.main] bad command', function(assert) {
+  var parsed = Object.assign({}, base, { command: 'hibbity' });
+
+  cli.main(parsed, function(err) {
+    assert.equal(err.message, 'Error: invalid command\n\n' + parsed.help, 'expected error message');
+    assert.end();
+  });
+});
+
+test('[cli.main] no environment', function(assert) {
+  var parsed = Object.assign({}, base, { environment: undefined });
+
+  cli.main(parsed, function(err) {
+    assert.equal(err.message, 'Error: missing environment\n\n' + parsed.help, 'expected error message');
+    assert.end();
+  });
+});
+
+test('[cli.main] no template path (create)', function(assert) {
+  var parsed = Object.assign({}, base, { templatePath: undefined });
+
+  cli.main(parsed, function(err) {
+    assert.equal(err.message, 'Error: missing templatePath\n\n' + parsed.help, 'expected error message');
+    assert.end();
+  });
+});
+
+test('[cli.main] no template path (info)', function(assert) {
+  sinon.stub(cfnConfig, 'commands', function() {
+    return { info: function() { Array.from(arguments).pop()(); } };
+  });
+
+  var parsed = Object.assign({}, base, { command: 'info', templatePath: undefined });
+
+  cli.main(parsed, function(err) {
+    assert.ifError(err, 'success');
+    cfnConfig.commands.restore();
+    assert.end();
+  });
+});
+
+test('[cli.main] create', function(assert) {
+  sinon.stub(cfnConfig, 'commands', function(options) {
+    assert.deepEqual(options, base.options, 'provided commands constructor with correct options');
+    return {
+      create: function(suffix, templatePath, overrides, callback) {
+        assert.equal(suffix, base.environment, 'provides correct suffix');
+        assert.equal(templatePath, base.templatePath, 'provides correct templatePath');
+        assert.deepEqual(overrides, base.overrides, 'provides correct overrides');
+        callback();
+      }
+    };
+  });
+
+  cli.main(base, function(err) {
+    assert.ifError(err, 'success');
+    cfnConfig.commands.restore();
+    assert.end();
+  });
+});
+
+test('[cli.main] update', function(assert) {
+  sinon.stub(cfnConfig, 'commands', function(options) {
+    assert.deepEqual(options, base.options, 'provided commands constructor with correct options');
+    return {
+      update: function(suffix, templatePath, overrides, callback) {
+        assert.equal(suffix, base.environment, 'provides correct suffix');
+        assert.equal(templatePath, base.templatePath, 'provides correct templatePath');
+        assert.deepEqual(overrides, base.overrides, 'provides correct overrides');
+        callback();
+      }
+    };
+  });
+
+  var parsed = Object.assign({}, base, { command: 'update' });
+
+  cli.main(parsed, function(err) {
+    assert.ifError(err, 'success');
+    cfnConfig.commands.restore();
+    assert.end();
+  });
+});
+
+test('[cli.main] delete', function(assert) {
+  sinon.stub(cfnConfig, 'commands', function(options) {
+    assert.deepEqual(options, base.options, 'provided commands constructor with correct options');
+    return {
+      delete: function(suffix, overrides, callback) {
+        assert.equal(suffix, base.environment, 'provides correct suffix');
+        assert.deepEqual(overrides, base.overrides, 'provides correct overrides');
+        callback();
+      }
+    };
+  });
+
+  var parsed = Object.assign({}, base, { command: 'delete' });
+
+  cli.main(parsed, function(err) {
+    assert.ifError(err, 'success');
+    cfnConfig.commands.restore();
+    assert.end();
+  });
+});
+
+test('[cli.main] info', function(assert) {
+  sinon.stub(cfnConfig, 'commands', function(options) {
+    assert.deepEqual(options, base.options, 'provided commands constructor with correct options');
+    return {
+      info: function(suffix, resources, callback) {
+        assert.equal(suffix, base.environment, 'provides correct suffix');
+        assert.deepEqual(resources, base.options.extended, 'provides correct resources boolean');
+        callback();
+      }
+    };
+  });
+
+  var parsed = Object.assign({}, base, { command: 'info' });
+
+  cli.main(parsed, function(err) {
+    assert.ifError(err, 'success');
+    cfnConfig.commands.restore();
+    assert.end();
+  });
+});
+
+test('[cli.main] save (with kms)', function(assert) {
+  sinon.stub(cfnConfig, 'commands', function(options) {
+    assert.deepEqual(options, base.options, 'provided commands constructor with correct options');
+    return {
+      save: function(suffix, kms, callback) {
+        assert.equal(suffix, base.environment, 'provides correct suffix');
+        assert.deepEqual(kms, base.options.kms, 'provides correct kms boolean');
+        callback();
+      }
+    };
+  });
+
+  var parsed = Object.assign({}, base, { command: 'save' });
+  parsed.options.kms = true;
+
+  cli.main(parsed, function(err) {
+    assert.ifError(err, 'success');
+    cfnConfig.commands.restore();
+    assert.end();
+  });
+});
+
+test('[cli.main] save (without kms)', function(assert) {
+  sinon.stub(cfnConfig, 'commands', function(options) {
+    assert.deepEqual(options, base.options, 'provided commands constructor with correct options');
+    return {
+      save: function(suffix, kms, callback) {
+        assert.equal(suffix, base.environment, 'provides correct suffix');
+        callback();
+      }
+    };
+  });
+
+  var parsed = Object.assign({}, base, { command: 'save' });
+
+
+  cli.main(parsed, function(err) {
+    assert.ifError(err, 'success');
+    cfnConfig.commands.restore();
+    assert.end();
+  });
+});
