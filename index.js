41deb0bc22d33d392d3434cb0f4d319b6a975365
Ryan Clark <clark@mapbox.com>
2018-01-23 15:13:16 -0700

    adds preauth function (#143)

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-module.exports = {
+var AWS = require('aws-sdk');
+
+var cfnConfig = {
   actions: require('./lib/actions'),
   commands: require('./lib/commands'),
   lookup: require('./lib/lookup'),
   prompt: require('./lib/prompt'),
   template: require('./lib/template')
+};
+
+module.exports = cfnConfig;
+
+module.exports.preauth = function(credentials) {
+  AWS.config.credentials = credentials;
+  return cfnConfig;
 };
