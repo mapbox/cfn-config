21b385fba5d30bdd1fbf4bce51eb77fb6b62e33a
ingalls <nick@ingalls.ca>
2022-01-25 07:57:39 -0700

    Modernify

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const AWS = require('aws-sdk');
 
 const cfnConfig = {
-  actions: require('./lib/actions'),
-  commands: require('./lib/commands'),
-  lookup: require('./lib/lookup'),
-  prompt: require('./lib/prompt'),
-  template: require('./lib/template')
+    actions: require('./lib/actions'),
+    commands: require('./lib/commands'),
+    lookup: require('./lib/lookup'),
+    prompt: require('./lib/prompt'),
+    template: require('./lib/template')
 };
 
 module.exports = cfnConfig;
 
 module.exports.preauth = (credentials) => {
-  AWS.config.credentials = credentials;
-  return cfnConfig;
+    AWS.config.credentials = credentials;
+    return cfnConfig;
 };
