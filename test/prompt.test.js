0ef4012294b19bca2d1169ecdee1b0f41f9d95f4
ingalls <nick@ingalls.ca>
2022-01-25 08:41:33 -0700

    Update prompt tests to expect promises

--- test/prompt.test.js
+++ test/prompt.test.js
@@ -3,139 +3,160 @@ const sinon = require('sinon');
 const inquirer = require('inquirer');
 const prompt = require('../lib/prompt');
 
-test('[prompt.confirm] single-line, confirm', function(assert) {
-  sinon.stub(inquirer, 'prompt').callsFake(function(questions) {
-    assert.deepEqual(questions, {
-      default: true,
-      type: 'confirm',
-      name: 'confirmation',
-      message: 'confirm?'
-    }, 'inquirer called with correct question');
+test('[prompt.confirm] single-line, confirm', async (t) => {
+    sinon.stub(inquirer, 'prompt').callsFake((questions) => {
+        t.deepEqual(questions, {
+            default: true,
+            type: 'confirm',
+            name: 'confirmation',
+            message: 'confirm?'
+        }, 'inquirer called with correct question');
 
-    return Promise.resolve({ confirmation: true });
-  });
+        return Promise.resolve({ confirmation: true });
+    });
 
-  prompt.confirm('confirm?', function(err, ready) {
-    assert.ifError(err, 'success');
-    assert.ok(ready, 'received user confirmation');
+    try {
+        const ready = prompt.confirm('confirm?');
+        t.ok(ready, 'received user confirmation');
+    } catch (err) {
+        t.error(err);
+    }
+
     inquirer.prompt.restore();
-    assert.end();
-  });
+    t.end();
 });
 
-test('[prompt.confirm] single-line, false default', function(assert) {
-  sinon.stub(inquirer, 'prompt').callsFake(function(questions) {
-    assert.deepEqual(questions, {
-      type: 'confirm',
-      name: 'confirmation',
-      message: 'confirm?',
-      default: false
-    }, 'inquirer called with correct question');
+test('[prompt.confirm] single-line, false default', async (t) => {
+    sinon.stub(inquirer, 'prompt').callsFake((questions) => {
+        t.deepEqual(questions, {
+            type: 'confirm',
+            name: 'confirmation',
+            message: 'confirm?',
+            default: false
+        }, 'inquirer called with correct question');
 
-    return Promise.resolve({ confirmation: true });
-  });
+        return Promise.resolve({ confirmation: true });
+    });
 
-  prompt.confirm('confirm?', false, function(err, ready) {
-    assert.ifError(err, 'success');
-    assert.ok(ready, 'received user confirmation');
+    try {
+        const ready = await prompt.confirm('confirm?', false);
+        t.ok(ready, 'received user confirmation');
+    } catch (err) {
+        t.error(err);
+    }
+
     inquirer.prompt.restore();
-    assert.end();
-  });
+    t.end();
 });
 
-test('[prompt.confirm] multi-line, reject', function(assert) {
-  sinon.stub(inquirer, 'prompt').callsFake(function(questions) {
-    assert.deepEqual(questions, {
-      default: true,
-      type: 'confirm',
-      name: 'confirmation',
-      message: 'confirm?'
-    }, 'inquirer called with correct question');
+test('[prompt.confirm] multi-line, reject', async (t) => {
+    sinon.stub(inquirer, 'prompt').callsFake((questions) => {
+        t.deepEqual(questions, {
+            default: true,
+            type: 'confirm',
+            name: 'confirmation',
+            message: 'confirm?'
+        }, 'inquirer called with correct question');
 
-    return Promise.resolve({ confirmation: false });
-  });
+        return Promise.resolve({ confirmation: false });
+    });
 
-  prompt.confirm('title\nexplanation\nconfirm?', function(err, ready) {
-    assert.ifError(err, 'success');
-    assert.notOk(ready, 'received user confirmation');
+    try {
+        const ready = await prompt.confirm('title\nexplanation\nconfirm?');
+        t.notOk(ready, 'received user confirmation');
+    } catch (err) {
+        t.error(err);
+    }
+
     inquirer.prompt.restore();
-    assert.end();
-  });
+    t.end();
 });
 
-test('[prompt.configuration] success', function(assert) {
-  sinon.stub(inquirer, 'prompt').callsFake(function(questions) {
-    assert.deepEqual(questions, {
-      type: 'list',
-      name: 'config',
-      message: 'Saved configurations',
-      choices: ['a', 'b', 'New configuration']
-    }, 'inquirer called with correct question');
+test('[prompt.configuration] success', async (t) => {
+    sinon.stub(inquirer, 'prompt').callsFake((questions) => {
+        t.deepEqual(questions, {
+            type: 'list',
+            name: 'config',
+            message: 'Saved configurations',
+            choices: ['a', 'b', 'New configuration']
+        }, 'inquirer called with correct question');
 
-    return Promise.resolve({ config: 'a' });
-  });
+        return Promise.resolve({ config: 'a' });
+    });
 
-  prompt.configuration(['a', 'b'], function(err, config) {
-    assert.ifError(err, 'success');
-    assert.equal(config, 'a', 'received user selection');
+    try {
+        const config = await prompt.configuration(['a', 'b']);
+        t.equal(config, 'a', 'received user selection');
+    } catch (err) {
+        t.error(err);
+    }
+
     inquirer.prompt.restore();
-    assert.end();
-  });
+    t.end();
 });
 
-test('[prompt.parameters] success', function(assert) {
-  sinon.stub(inquirer, 'prompt').callsFake(function(questions) {
-    assert.deepEqual(questions, {
-      questions: 'passed through'
-    }, 'passes through provided questions');
+test('[prompt.parameters] success', async (t) => {
+    sinon.stub(inquirer, 'prompt').callsFake((questions) => {
+        t.deepEqual(questions, {
+            questions: 'passed through'
+        }, 'passes through provided questions');
 
-    return Promise.resolve({ questions: 'answers', needsStringify: 6 });
-  });
+        return Promise.resolve({ questions: 'answers', needsStringify: 6 });
+    });
 
-  prompt.parameters({ questions: 'passed through' }, function(err, answers) {
-    assert.ifError(err, 'success');
-    assert.deepEqual(answers, { questions: 'answers', needsStringify: '6' }, 'received user responses');
+    try {
+        const answers = await prompt.parameters({ questions: 'passed through' });
+        t.deepEqual(answers, { questions: 'answers', needsStringify: '6' }, 'received user responses');
+    } catch (err) {
+        t.error(err);
+    }
+
     inquirer.prompt.restore();
-    assert.end();
-  });
+    t.end();
 });
 
-test('[prompt.input] no default value', function(assert) {
-  sinon.stub(inquirer, 'prompt').callsFake(function(questions) {
-    assert.deepEqual(questions, {
-      type: 'input',
-      name: 'data',
-      default: undefined,
-      message: 'what:'
-    }, 'asks the right question');
+test('[prompt.input] no default value', async (t) => {
+    sinon.stub(inquirer, 'prompt').callsFake((questions) => {
+        t.deepEqual(questions, {
+            type: 'input',
+            name: 'data',
+            default: undefined,
+            message: 'what:'
+        }, 'asks the right question');
 
-    return Promise.resolve({ data: 'answers' });
-  });
+        return Promise.resolve({ data: 'answers' });
+    });
 
-  prompt.input('what:', function(err, response) {
-    assert.ifError(err, 'success');
-    assert.equal(response, 'answers', 'received user response');
+    try {
+        const response = await prompt.input('what:');
+        t.equal(response, 'answers', 'received user response');
+    } catch (err) {
+        t.error(err);
+    }
+
     inquirer.prompt.restore();
-    assert.end();
-  });
+    t.end();
 });
 
-test('[prompt.input] with default value', function(assert) {
-  sinon.stub(inquirer, 'prompt').callsFake(function(questions) {
-    assert.deepEqual(questions, {
-      type: 'input',
-      name: 'data',
-      default: 'hibbity',
-      message: 'what:'
-    }, 'asks the right question');
+test('[prompt.input] with default value', async (t) => {
+    sinon.stub(inquirer, 'prompt').callsFake((questions) => {
+        t.deepEqual(questions, {
+            type: 'input',
+            name: 'data',
+            default: 'hibbity',
+            message: 'what:'
+        }, 'asks the right question');
 
-    return Promise.resolve({ data: 'answers' });
-  });
+        return Promise.resolve({ data: 'answers' });
+    });
 
-  prompt.input('what:', 'hibbity', function(err, response) {
-    assert.ifError(err, 'success');
-    assert.equal(response, 'answers', 'received user response');
+    try {
+        const response = await prompt.input('what:', 'hibbity');
+        t.equal(response, 'answers', 'received user response');
+    } catch (err) {
+        t.error(err);
+    }
+
     inquirer.prompt.restore();
-    assert.end();
-  });
+    t.end();
 });
